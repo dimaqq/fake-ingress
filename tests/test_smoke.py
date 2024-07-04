e2bfda2658cf64475180de2d708fab3b50a38efb
Dima Tisnek <dimaqq@gmail.com>
2024-07-04 15:14:38 +0900

    happy test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,8 +1,19 @@
 import scenario
-
 from fake_ingress.charm import FakeIngressCharm
 
+META = {
+    "name": "fake-ingress",
+    "provides": {
+        "ingress": {
+            "interface": "ingress"
+        }
+    }
+}
 
 def test_smoke():
-    ctx = scenario.Context(FakeIngressCharm, meta=dict(name="fake-ingress"))
-    res = ctx.run(ctx.on.start(), scenario.State(leader=True))
+    ctx = scenario.Context(FakeIngressCharm, meta=META)
+    in_ = scenario.State(
+        leader=True,
+        relations=[scenario.Relation(endpoint="ingress", interface="ingress", remote_app_name="dummy")],
+    )
+    ctx.run(ctx.on.start(), in_)
