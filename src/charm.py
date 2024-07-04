dd1cc92f47ccd0528c70d8f8f981da8e1d4534e3
Dima Tisnek <dimaqq@gmail.com>
2024-07-04 13:40:35 +0900

    happy ruff

--- src/charm.py
+++ src/charm.py
@@ -2,18 +2,34 @@
 import logging
 
 import ops
-
 from charms.traefik_k8s.v2.ingress import IngressPerAppProvider
+from ops.charm import RelationEvent
 
 
 class FakeIngressCharm(ops.CharmBase):
     """Charm the service."""
 
-    def __init__(self, framework: ops.Framework):
-        super().__init__(framework)
+    def __init__(self, *args):
+        super().__init__(*args)
         self.per_app = IngressPerAppProvider(charm=self)
-        framework.observe(self.on["httpbin"].pebble_ready, self._on_httpbin_pebble_ready)
-        framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.per_app.on.data_provided, self.provided)
+        self.farmework.observe(self.per_app.on.data_removed, self.removed)
+
+    def provided(self, event: RelationEvent):
+        logging.warning("proivided raw %s", event.snapshot())
+        logging.warning("proivided %s",
+                        [
+                            event,
+                            event.app,
+                            event.unit,
+                            event.relation,
+                            event.relation.name,
+                            event.relation.id,
+                        ])
+        self.per_app.publish_url(event.relation, "http://lb.example/")
+
+    def removed(self, event: RelationEvent):
+        logging.warning("removed raw %s", event.snapshot())
 
 if __name__ == "__main__":  # pragma: nocover
     ops.main(FakeIngressCharm)  # type: ignore
