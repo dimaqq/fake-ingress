5959c5813a64bb20eb4aae1d0da3021114561392
Dima Tisnek <dimaqq@gmail.com>
2024-08-15 14:11:24 +0900

    wip

---
+++ src/fake_ingress/charm.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import logging
+
+import ops
+from charms.traefik_k8s.v2.ingress import IngressPerAppProvider
+from ops.charm import RelationEvent
+
+
+class FakeIngressCharm(ops.CharmBase):
+    """Charm the service."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.per_app = IngressPerAppProvider(charm=self)
+        self.framework.observe(self.per_app.on.data_provided, self.provided)
+        self.framework.observe(self.per_app.on.data_removed, self.removed)
+
+    def provided(self, event: RelationEvent):
+        logging.warning("proivided raw %s", event.snapshot())
+        logging.warning("proivided %s",
+                        [
+                            event,
+                            event.app,
+                            event.unit,
+                            event.relation,
+                            event.relation.name,
+                            event.relation.id,
+                        ])
+        self.per_app.publish_url(event.relation, "http://lb.example/")
+
+    def removed(self, event: RelationEvent):
+        logging.warning("removed raw %s", event.snapshot())
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(FakeIngressCharm)  # type: ignore
